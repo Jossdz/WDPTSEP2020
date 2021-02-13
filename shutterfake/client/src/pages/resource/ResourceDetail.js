f9e50e86b54bb5517e2ca7eb91689acb47f19afa
@jossdz <carloscorrea.dev@gmail.com>
2021-02-13 15:27:00 -0600

    resource detail

--- shutterfake/client/src/pages/resource/ResourceDetail.js
+++ shutterfake/client/src/pages/resource/ResourceDetail.js
@@ -1,16 +1,67 @@
-import React from "react"
-import { Row, Col, Typography, Button } from "antd"
+import { useEffect, useState } from "react"
+import { Row, Col, Typography, Skeleton, Button, Tag } from "antd"
+import { getOneResource } from "../../services/resources"
 
-const { Title } = Typography
+const { Title, Text } = Typography
 
-function ResourceDetail({ match: { params } }) {
+function ResourceDetail({ match: { params }, history }) {
+  const [resource, setResource] = useState(null)
+
+  console.log(history)
+
+  useEffect(() => {
+    async function getResource() {
+      const { data: resource } = await getOneResource(params.resourceId)
+      setResource(resource)
+    }
+    getResource()
+  }, [])
+
+  function returnResourceTag(type) {
+    switch (type) {
+      case "IMAGE":
+        return <Tag color='blue'>Image</Tag>
+      case "VIDEO":
+        return <Tag color='magenta'>Video</Tag>
+      case "AUDIO":
+        return <Tag color='geekblue'>Audio</Tag>
+    }
+  }
+
   console.log(params)
-  return (
+  return resource ? (
     <Row gutter={[16, 16]}>
       <Col span={24}>
-        <Title>{/* {TODO: Nombre del recurso} */}</Title>
+        <Title>{resource.name}</Title>
+      </Col>
+      <Col xs={{ span: 24 }} md={{ span: 12 }}>
+        <img src={resource.url} alt={resource.name} style={{ width: "100%" }} />
+      </Col>
+      <Col xs={{ span: 24 }} md={{ span: 12 }}>
+        <Title level={2}>
+          Price: <Text type='secondary'>${resource.price}.00</Text>
+        </Title>
+        <p>
+          <Text type='secondary'>{resource.description}</Text>
+        </p>
+        {returnResourceTag(resource.type)}
+        <Button style={{ margin: "5px 0" }} block type='primary' size='large'>
+          Buy
+        </Button>
+        <Button style={{ margin: "5px 0" }} block type='dashed'>
+          Edit
+        </Button>
+        <Button
+          style={{ margin: "5px 0" }}
+          block
+          onClick={() => history.goBack()}
+        >
+          Back
+        </Button>
       </Col>
     </Row>
+  ) : (
+    <Skeleton active />
   )
 }
 
